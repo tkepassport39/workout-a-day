5216e26ada402fd5afdd1b1019f68988ad9c478d
Anthony Vanegas <avanegas04@gmail.com>
2022-04-29 13:26:22 -0400

    Adding focus instead of muscle groups

--- workout-web/classes.js
+++ workout-web/classes.js
@@ -1,38 +1,64 @@
+const { Module } = require("module")
+
+const numOfExercise = 3
+
 class User {
 
-    static async create() { }
-    static async get(email){ }
-    async update(){ }
-    async delete(){ }
+    static async create() {}
+    static async get(email){}
+    async update(){}
+    async delete(){}
 }
 
 class UserExerciseWorkout {
-    static async create(user, exercise, workout) { }
-    static async get(){ }
-    async update(){ }
-    async delete(){ }
+    static async create(user, exercise, workout){
+        console.log(JSON.stringify(user, null, 2))
+        console.log(JSON.stringify(exercise, null, 2))
+        console.log(JSON.stringify(workout, null, 2))
+    }
+    static async get(){}
+    async update(){}
+    async delete(){}
+    static async find(user, exercise, workout){}
 }
 
 class Workout {
-    static async create() { }
-    static async get(){ }
-    async update(){ }
-    async delete(){ }
+    constructor(id, name){
+        this.id = id
+        this.name = name
+    }
+
+    static async create(){
+        // create workout in DB
+        const dbID = 1
+        // return new workout id
+        return new Workout(dbID, "Anthony")
+    }
+    static async get(){}
+    static async getLastWorkoutForUser(user){}
+    async update(){
+        console.log(this.workoutName)
+    }
+    async delete(){}
 }
 
 class Exercise {
-    static async create() { }
-    static async get(){ }
-    async update(){ }
-    async delete(){ }
+    static async create(){}
+    static async get(){}
+    static async getFocus(focus){}
+    async update(){}
+    async delete(){}
 }
 
-async function getTodayWorkout(userEmail, muscles) {
+async function getTodayWorkout(userEmail, focus) {
     // need to get user from User class
     const user = await User.get(userEmail)
 
+    // get the last workout for the user
+    const lastWorkout = await Workout.getLastWorkoutForUser(user)
+
     // then get 6 exercises for 2 muscle groups
-    const exercises = await getExercisesForMuscleGroup(muscles)
+    const exercises = await getExercisesForFocusGroup(focus , numOfExercise, lastWorkout)
 
     // group exercises into a workout for the day
     const workout = await Workout.create()
@@ -47,7 +73,13 @@ async function getTodayWorkout(userEmail, muscles) {
     return workout
 }
 
-async function getExercisesForMuscleGroup(muscles){ }
+async function getExercisesForFocusGroup(focus, count, lastWorkout){
+    const exercises = Exercise.getFocus(focus)
+
+    // select three
+
+    return exercises
+}
 
 async function saveReps(user, exercise, workout){
     // pull the workout
@@ -57,5 +89,30 @@ async function saveReps(user, exercise, workout){
     // get the exercise
 
     // update reps in UserExerciseWorkout.Update() 
+
 }
 
+async function saveSets(user, exercise, workout){
+    // pull the workout
+
+    // get the user
+
+    // get the exercise
+
+    // update sets in UserExerciseWorkout.Update() 
+}
+
+async function saveWeight(user, exercise, workout){
+    // pull the workout
+
+    // get the user
+
+    // get the exercise
+
+    // update weight in UserExerciseWorkout.Update() 
+}
+
+module.exports = { 
+    getTodayWorkout,
+    Workout,
+}
