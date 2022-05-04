0dcf108add623ae268806fbbad7aecc2cf51663f
Anthony Vanegas <avanegas04@gmail.com>
2022-05-04 13:34:49 -0400

    get user create and get working

--- workout-web/classes.js
+++ workout-web/classes.js
@@ -1,14 +1,40 @@
-const { Module } = require("module")
+const db_exercise = require("./data/exercise")
+const db_focus = require("./data/focus")
+const db_user_exercise_workout = require("./data/user-exercise-workout")
+const db_user = require("./data/user")
+const db_workout = require("./data/workout")
 
 const numOfExercise = 3
 
 class User {
 
-    static async create() {}
-    static async get(email){}
+    static async create(email) {
+        // generate a new ID for the user
+        const newID = db_user.length
+
+        // create user and add to USER DB
+        const newUser = {
+            "id" : newID,
+            "email" : email
+        }
+
+        db_user.push(newUser)
+
+        // return the new user object
+        return newUser
+    }
+    static async get(email){
+        return db_user.find(element => element.email == email);
+    }
     async update(){}
     async delete(){}
 }
+
+User.get("melissa@gmail.com").then(console.log)
+
+User.create("kevin@hotmail.com").then(console.log)
+
+console.log(db_user.length)
 
 class UserExerciseWorkout {
     static async create(user, exercise, workout){
