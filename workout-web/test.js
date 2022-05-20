329198f1c7c177abdd1719361bd49ba07f50bc5f
Anthony Vanegas <avanegas04@gmail.com>
2022-05-20 12:06:57 -0400

    Adding Test unit for this code

--- workout-web/test.js
+++ workout-web/test.js
@@ -2,20 +2,16 @@ const { User , getTodayWorkout } = require('./classes')
 
 // test User
 async function testUser(){
-
     const melissa = await User.get("melissa@gmail.com")
     console.log(melissa)
     
     const kevin = await User.create("kevin@hotmail.com")
     console.log(kevin)
 }
-
 testUser()
 
 async function testGetTodayWorkout(){
-
     const workoutResult = await getTodayWorkout('melissa@gmail.com', ["bicep", "tricep"])
     console.log(workoutResult)
 }
-
 testGetTodayWorkout()
